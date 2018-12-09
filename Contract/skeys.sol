34c8fafad103434853c7bb74aa666b95267c2cb4
srihari kapu <sriharikapu163@gmail.com>
2018-12-09 20:07:05 +0800

    Update skeys.sol

--- Contract/skeys.sol
+++ Contract/skeys.sol
@@ -1,11 +1,5 @@
 pragma solidity 0.4.24;
 
-/**
-        @dev
-        author: srihari kapu
-        company: sustainy.org
-*/
-
 library SafeMath {
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
       if (a == 0) {
